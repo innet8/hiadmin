87f11f2717eb07489959daacc81db110dbac8eea
Chuaer <1063195568@qq.com>
2023-02-02 17:21:57 +0800

    fix: 实例化对象

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
+// vuex
 import { createPinia } from 'pinia'
+// router
+import router from './router'
 // css
 import './assets/css/main.css'
 // App
 import App from './App.vue'
-// router
-import router from './router'
-//
+// 实例化对象
 createApp(App).use(createPinia()).use(router).mount('#app')
