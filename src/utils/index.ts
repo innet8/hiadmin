2589d370235a6475b481b773f5ab0e033e5f4da1
Chuaer <1063195568@qq.com>
2023-02-03 16:18:25 +0800

    fix: 修复token的key

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 import type { LocaleType } from '../types/config'
 
 //
-export const TOKEN = `${import.meta.env.APP_NAME}_token`.toUpperCase()
+export const TOKEN = 'hiadmin_token'.toUpperCase()
 // 获取token
 export function getToken() {
   return localStorage.getItem(TOKEN)
@@ -12,7 +12,7 @@ export function setToken(data: string) {
 }
 
 //
-export const LANGUAGE = `${import.meta.env.APP_NAME}_language`.toUpperCase()
+export const LANGUAGE = 'hiadmin_language'.toUpperCase()
 // 获取语言
 export function getLanguage(): LocaleType {
   return localStorage.getItem(LANGUAGE) as LocaleType
