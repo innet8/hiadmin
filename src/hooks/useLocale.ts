bd1129f215266313446c6e1c0dff226222160169
Chuaer <1063195568@qq.com>
2023-02-06 16:51:30 +0800

    fix: 修复语言包捕获错误

--- src/hooks/useLocale.ts
+++ src/hooks/useLocale.ts
@@ -2,42 +2,34 @@ import { unref } from 'vue'
 import { i18n } from '../locales/index'
 import { setLanguage } from '../utils'
 import type { LocaleType } from '../types/config'
-
-export const loadLocalePool: LocaleType[] = []
-
-function setI18nLanguage(locale: LocaleType) {
+// 设置语言
+export function setI18nLanguage(locale: LocaleType) {
   if (i18n.mode === 'legacy') {
     i18n.global.locale = locale
   } else {
     ;(i18n.global.locale as any).value = locale
   }
 }
-
+// 切换语言
 export async function changeLocale(locale: LocaleType) {
-  // 设置本地用户语言
-  setLanguage(locale)
   //
   const globalI18n = i18n.global
-  const currentLocale = unref(globalI18n.locale)
   // 语言未变化
-  if (currentLocale === locale) {
-    return locale
+  if (unref(globalI18n.locale) === locale) return
+  // 动态加载语言包
+  if (!globalI18n.availableLocales?.includes(locale)) {
+    try {
+      // 获取语言包
+      const defaultLocal = await import(`../locales/lang/${locale}.ts`)
+      const message = defaultLocal?.default ?? {}
+      // 重新设置语言包
+      globalI18n.setLocaleMessage(locale, message)
+    } catch (error) {
+      console.log(`${locale}.ts的语言包不存在`)
+    }
   }
-  // 已经存在语言包
-  if (globalI18n.availableLocales?.includes(locale)) {
-    setI18nLanguage(locale)
-    return locale
-  }
-  try {
-    // 获取语言包
-    const defaultLocal = await import(`../locales/lang/${locale}.ts`)
-    const message = defaultLocal?.default ?? {}
-    // 重新设置语言包
-    globalI18n.setLocaleMessage(locale, message)
-    //
-    setI18nLanguage(locale)
-    return locale
-  } catch (error) {
-    console.log(`${locale}.ts的语言包不存在`)
-  }
+  // 设置语言
+  setI18nLanguage(locale)
+  // 设置本地用户语言
+  setLanguage(locale)
 }
