1efd73d1634a9519eed31bc1dbf1af1d18936bfb
Chuaer <1063195568@qq.com>
2023-02-06 18:10:29 +0800

    fix: 修复编译报错

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -42,18 +42,9 @@ const handleSignIn = () => {
         <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
       </p>
     </div>
-
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-        <form
-          class="space-y-6"
-          :onsubmit="
-            (e) => {
-              e.preventDefault()
-              return false
-            }
-          "
-        >
+        <form class="space-y-6" :onsubmit="() => false">
           <div>
             <label for="email" class="block text-sm font-medium text-gray-700">{{ $t('emailAddress') }}</label>
             <div class="mt-1">
@@ -62,19 +53,7 @@ const handleSignIn = () => {
                 name="email"
                 type="email"
                 autocomplete="email"
-                class="
-                  block
-                  w-full
-                  appearance-none
-                  rounded-md
-                  border border-gray-300
-                  px-3
-                  py-2
-                  placeholder-gray-400
-                  shadow-sm
-                  focus:border-indigo-500 focus:outline-none focus:ring-indigo-500
-                  sm:text-sm
-                "
+                class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
               />
             </div>
           </div>
@@ -87,19 +66,7 @@ const handleSignIn = () => {
                 name="password"
                 type="password"
                 autocomplete="current-password"
-                class="
-                  block
-                  w-full
-                  appearance-none
-                  rounded-md
-                  border border-gray-300
-                  px-3
-                  py-2
-                  placeholder-gray-400
-                  shadow-sm
-                  focus:border-indigo-500 focus:outline-none focus:ring-indigo-500
-                  sm:text-sm
-                "
+                class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
               />
             </div>
           </div>
@@ -118,22 +85,7 @@ const handleSignIn = () => {
           <div>
             <button
               type="submit"
-              class="
-                flex
-                w-full
-                justify-center
-                rounded-md
-                border border-transparent
-                bg-indigo-600
-                py-2
-                px-4
-                text-sm
-                font-medium
-                text-white
-                shadow-sm
-                hover:bg-indigo-700
-                focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
-              "
+              class="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               @click="handleSignIn"
             >
               {{ $t('signIn') }}
