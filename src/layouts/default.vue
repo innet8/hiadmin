ddcf2063c1e02c012cc9b1fe6160930a592782e6
Chuaer <1063195568@qq.com>
2023-02-07 09:05:07 +0800

    fix: 去掉底部

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import Header from '../components/Header.vue'
-import Navigation from '../components/Navigation.vue'
-import Footer from '../components/Footer.vue'
+import Menu from '../components/Menu.vue'
+// import Footer from '../components/Footer.vue'
 </script>
 
 <template>
   <!-- 头部 -->
   <Header />
   <!-- 导航 -->
-  <Navigation />
+  <Menu />
   <!-- 主体 -->
   <main class="relative"><router-view /></main>
   <!-- 尾部 -->
-  <Footer />
+  <!-- <Footer /> -->
 </template>
