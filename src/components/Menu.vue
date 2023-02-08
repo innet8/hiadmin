51f6c18d54b1cad6b3bcd8216ecc159273fd1a41
Chuaer <1063195568@qq.com>
2023-02-08 13:59:48 +0800

    fix: 修复菜单组件名

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -4,7 +4,7 @@
 <template>
   <aside>
     <hr />
-    Navigation<br />
+    Menu<br />
     <router-link to="/admin/dashboard" class="text-blue-500">dashboard</router-link><br />
     <router-link to="/admin/settings" class="text-blue-500">settings</router-link><br />
     <hr />
