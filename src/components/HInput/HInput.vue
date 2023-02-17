cf23e367f8fbbfa1a90eb40aba91a870a10ae1be
Daniel Morrison <1013921026@qq.com>
2023-02-17 17:17:30 +0800

    feat: [HButton] 按钮基础样式；[HInput] 重置输入框按钮

--- src/components/HInput/HInput.vue
+++ src/components/HInput/HInput.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class='h-input-wrapper group'>
-    <input
-      :type="nativeType"
-      :autocomplete="autocomplete"
-      class="h-input"
-      :class="customClass"
-      :value="modelValue"
-    />
+  <div class="h-input-container">
+    <div class="h-input-wrapper group">
+      <input
+        :type="nativeType"
+        :autocomplete="autocomplete"
+        class="h-input"
+        :class="customClass"
+        :disabled="disabled"
+        :value="modelValue"
+        @input="$emit('update:modelValue', $event.target.value)"
+      />
+      <div class="clearable-btn" @click='clearBtnClick' v-if="clearable && modelValue !== ''">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-3 w-3"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg>
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang='ts' setup>
+<script lang="ts" setup>
 import {componentSizeMapProxy, componentSizes} from '../constant/size'
 
+const emit = defineEmits(['update:modelValue'])
 const props = defineProps({
   autocomplete: {
     type: String,
@@ -27,6 +48,11 @@ const props = defineProps({
   customClass: {
     type: String,
     default: '',
+    required: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
     required: false,
   },
   id: {
@@ -45,7 +71,7 @@ const props = defineProps({
     required: false,
   },
   modelValue: {
-    type: () => [String, Number, Object],
+    type: [String, Number, Object],
     default: '',
   },
   nativeType: {
@@ -59,16 +85,25 @@ const props = defineProps({
     required: false,
   }
 })
+
+const clearBtnClick = () => {
+  emit('update:modelValue', '')
+}
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
 .h-input-wrapper {
-  @apply flex w-full sm:text-sm rounded-md border border-gray-300 transition-colors duration-150 ease-in-out shadow-sm
-  focus-within:shadow-md focus-within:border-indigo-500 focus-within:outline-none focus-within:ring-indigo-500;
+  @apply relative pr-2 flex justify-center items-center w-full sm:text-sm rounded-md border border-gray-300
+  transition-colors duration-150 ease-in-out shadow-sm focus-within:shadow-md focus-within:border-indigo-500
+  focus-within:outline-none focus-within:ring-indigo-500;
 
   .h-input {
     @apply relative px-3 py-2 w-full appearance-none rounded-md border-0 ring-0 border-transparent
     placeholder-gray-400 z-[0];
+  }
+
+  .clearable-btn {
+    @apply relative h-fit text-gray-500 bg-gray-200 rounded-full cursor-pointer z-[1]
   }
 }
 </style>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -6,6 +6,7 @@ import type { LocaleType } from '../../types/config'
 import { changeLocale } from '../../hooks/useLocale'
 import HInput from '../../components/HInput/HInput.vue'
 import { ref } from 'vue'
+import HButton from '../../components/HButton/HButton.vue'
 // 语言列表
 const localeList: { lang: LocaleType; text: string }[] = [
   {
@@ -63,14 +64,14 @@ const handleSignIn = () => {
 <!--                autocomplete="email"-->
 <!--                class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"-->
 <!--              />-->
-              <HInput v-model="loginForm.username" native-type="email" autocomplete="email"></HInput>
+              <HInput v-model="loginForm.username" clearable native-type="email" autocomplete="email"></HInput>
             </div>
           </div>
 
           <div>
             <label for="password" class="block text-sm font-medium text-gray-700">{{ $t('password') }}</label>
             <div class="mt-1">
-              <HInput v-model="loginForm.password" native-type="password"></HInput>
+              <HInput v-model="loginForm.password" clearable native-type="password"></HInput>
             </div>
           </div>
 
@@ -86,13 +87,16 @@ const handleSignIn = () => {
           </div>
 
           <div>
-            <button
-              type="submit"
-              class="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              @click="handleSignIn"
-            >
+<!--            <button-->
+<!--              type="submit"-->
+<!--              class="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"-->
+<!--              @click="handleSignIn"-->
+<!--            >-->
+<!--              {{ $t('signIn') }}-->
+<!--            </button>-->
+            <HButton @click="handleSignIn">
               {{ $t('signIn') }}
-            </button>
+            </HButton>
           </div>
         </form>
 
@@ -132,8 +136,18 @@ const handleSignIn = () => {
             </div>
 
             <div>
-              <a href="#" class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50">
-                <span class="sr-only">Sign in with GitHub</span>
+<!--              <a href="#" class="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50">-->
+<!--                <span class="sr-only">Sign in with GitHub</span>-->
+<!--                <svg class="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">-->
+<!--                  <path-->
+<!--                    fill-rule="evenodd"-->
+<!--                    d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"-->
+<!--                    clip-rule="evenodd"-->
+<!--                  />-->
+<!--                </svg>-->
+<!--              </a>-->
+              <HButton plain>
+<!--                <span class="sr-only">Sign in with GitHub</span>-->
                 <svg class="h-5 w-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                   <path
                     fill-rule="evenodd"
@@ -141,7 +155,7 @@ const handleSignIn = () => {
                     clip-rule="evenodd"
                   />
                 </svg>
-              </a>
+              </HButton>
             </div>
           </div>
         </div>
