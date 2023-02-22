18926fc7cddae80575bf4d3be6fa4a29c44c14df
Chuaer <1063195568@qq.com>
2023-02-22 18:22:53 +0800

    fix: 修复说明

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig(({ mode }) => {
         '/ws': {
           target: env.VITE_WEBSOCKET_URL,
           changeOrigin: true,
-          ws: true // 开启websocket
+          ws: true // 开启 websocket 代理
           // secure: false
           // rewrite: (path) => path.replace(/^\/ws/, '/ws/v1/')
         }
