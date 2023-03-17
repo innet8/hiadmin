bebeb67e0944dd67a1c0d1b56751dd0ea324a4e1
Chuaer <1063195568@qq.com>
2023-03-17 09:56:25 +0800

    feat: 升级插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite'
 import { resolve } from 'path'
+
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    plugins: [vue()],
+    plugins: [vue(), splitVendorChunkPlugin()],
     server: {
       host: '0.0.0.0', // 服务器ip地址
       port: +env.APP_PORT || 3000, // 本地端口
@@ -35,17 +37,25 @@ export default defineConfig(({ mode }) => {
       }
     },
     build: {
-      cssCodeSplit: true, // 如果设置为false，整个项目中的所有 CSS 将被提取到一个 CSS 文件中
-      sourcemap: false, // 构建后是否生成 source map 文件。如果为 true，将会创建一个独立的 source map 文件
+      // outDir: 'dist', // 输出目录
+      // assetsDir: 'assets', // 指定生成静态资源的存放路径
       target: 'modules', // 设置最终构建的浏览器兼容目标。默认值是一个 Vite 特有的值——'modules'  还可设置为 'es2015' 'es2016'等
-      chunkSizeWarningLimit: 550, // 单位kb  打包后文件大小警告的限制 (文件大于此此值会出现警告)
+      cssCodeSplit: true, // 如果设置为false，整个项目中的所有 CSS 将被提取到一个 CSS 文件中
+      sourcemap: 'production' !== mode, // 构建后是否生成 source map 文件。如果为 true，将会创建一个独立的 source map 文件
+      chunkSizeWarningLimit: 1000, // 单位kb  打包后文件大小警告的限制 (文件大于此此值会出现警告)
       assetsInlineLimit: 4096, // 单位字节（1024等于1kb） 小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求。设置为 0 可以完全禁用此项。
       minify: 'terser', // 'terser' 相对较慢，但大多数情况下构建后的文件体积更小。'esbuild' 最小化混淆更快但构建后的文件相对更大。
+      brotliSize: false, // 启用/禁用 brotli 压缩大小报告。压缩大型输出文件可能会很慢，因此禁用该功能可能会提高大型项目的构建性能。
       terserOptions: {
         compress: {
           drop_console: true, // 生产环境去除console
           drop_debugger: true // 生产环境去除debugger
         }
+      }
+    },
+    css: {
+      postcss: {
+        plugins: [autoprefixer]
       }
     }
   }
